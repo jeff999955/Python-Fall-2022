5b1479341a74ed1d1b0ba7ead50249c3329612c2
Jeff <jeff999955@gmail.com>
2022-12-04 22:30:07 +0800

    Modify main as question interface

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-class Question:
-    def __init__(self, prompt, answer):
-        self.prompt = prompt
-        self.answer = answer
-
-    def ask(self):
-        answer = input(self.prompt)
-        if answer == self.answer:
-            return True
-        return False
-
-
 question_list = []
 
 for question in question_list:
-    if question.ask():
+    question.ask()
+    if question.validate():
         print("Correct!")
     else:
         print("Incorrect!")
