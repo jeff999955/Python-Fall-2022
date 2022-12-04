28f218a385ec76c582cfc2882a616a0fa3d4463b
Jeff <jeff999955@gmail.com>
2022-12-04 22:45:16 +0800

    Add tutorial interface to questions

--- question.py
+++ question.py
@@ -1,8 +1,20 @@
+from typing import Iterable, Union
+
+
 class Question:
-    def __init__(self, prompt, answer):
+    def __init__(
+        self,
+        prompt: str,
+        answer: str,
+        tutorial: Union[str, Iterable[str]] = None,
+    ):
         self.prompt = prompt
         self.answer = answer
         self.response = None
+        if isinstance(tutorial, str):
+            self.tutorial = [tutorial]
+        else:
+            self.tutorial = [*tutorial]
 
     def ask(self):
         self.response = input(self.prompt)
@@ -10,10 +22,24 @@ class Question:
     def validate(self):
         return self.response == self.answer
 
+    def add_tutorial(self, link):
+        self.tutorial.append(link)
+
+    def show_tutorial(self):
+        print("Tutorial:")
+        for link in self.tutorial:
+            print(f"\t{link}")
+
 
 class MultipleChoiceQuestion(Question):
-    def __init__(self, prompt, answer, choices):
-        super().__init__(prompt, answer)
+    def __init__(
+        self,
+        prompt: str,
+        answer: str,
+        choices: Iterable[str],
+        tutorial: Union[str, Iterable[str]] = None,
+    ):
+        super(MultipleChoiceQuestion, self).__init__(prompt, answer, tutorial)
         self.choices = choices
         if self.answer not in self.choices:
             raise "Answer must be in choices"
