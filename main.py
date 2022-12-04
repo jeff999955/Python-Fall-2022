2ed6331b14049723d3fad46626efd0b02d703372
Jeff <jeff999955@gmail.com>
2022-12-04 22:34:33 +0800

    Add sample question

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-question_list = []
+from question import MultipleChoiceQuestion
+
+
+question_list = [
+    MultipleChoiceQuestion(
+        prompt="print(type(0.56)) 顯示的結果為何?",
+        answer="float",
+        choices=["int", "float", "str", "bool"],
+    ),
+]
 
 for question in question_list:
     question.ask()
