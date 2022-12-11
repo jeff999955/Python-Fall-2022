733d31c0d82fea59d4c865c4161f04b89b00d913
Jeff <jeff999955@gmail.com>
2022-12-11 23:26:30 +0800

    Remove tutorial from question

--- lesson.py
+++ lesson.py
@@ -1,25 +1,28 @@
+from typing import List
+
 from question import Question
 
 
 class Lesson:
     def __init__(self, name: str) -> None:
         self.name = name
-        self.instructions = []
-        self.questions = []
+        self._instructions: List[str] = []
+        self._questions: List[Question] = []
+        self._tutorials: List[str] = []
 
     def add_instruction(self, instruction: str):
-        self.instructions.append(instruction)
+        self._instructions.append(instruction)
 
     def show_instructions(self):
-        for instruction in self.instructions:
+        for instruction in self._instructions:
             print(instruction)
             input("Press enter to continue...")
 
     def add_question(self, question: Question):
-        self.questions.append(question)
+        self._questions.append(question)
 
     def show_questions(self):
-        for question in self.questions:
+        for question in self._questions:
             question.ask()
             if question.validate():
                 print("Correct!")
@@ -27,3 +30,29 @@ class Lesson:
                 print(f"Incorrect! The correct answer is {question.answer}.")
                 question.show_tutorial()
             input("Press enter to continue...")
+
+    def add_tutorial(self, link: str):
+        self._tutorials.append(link)
+
+    def show_tutorials(self):
+        print("Tutorial:")
+        for link in self._tutorials:
+            print(f"\t{link}")
+
+    def to_json(self):
+        return {
+            "name": self.name,
+            "instructions": self._instructions,
+            "questions": [question.to_json() for question in self._questions],
+            "tutorials": self._tutorials,
+        }
+
+    @staticmethod
+    def from_json(self, data) -> Lesson:
+        self.name = data["name"]
+        self._instructions = data["instructions"]
+        self._questions = [
+            Question.from_json(question) for question in data["questions"]
+        ]
+        self._tutorials = data["tutorials"]
+        return self
--- question.py
+++ question.py
@@ -6,29 +6,16 @@ class Question:
         self,
         prompt: str,
         answer: str,
-        tutorial: Union[str, Iterable[str]] = None,
-    ):
+    ) -> None:
         self.prompt = prompt
         self.answer = answer
         self.response = None
-        if isinstance(tutorial, str):
-            self.tutorial = [tutorial]
-        else:
-            self.tutorial = [*tutorial]
 
     def ask(self):
         self.response = input(self.prompt)
 
     def validate(self):
         return self.response == self.answer
-
-    def add_tutorial(self, link):
-        self.tutorial.append(link)
-
-    def show_tutorial(self):
-        print("Tutorial:")
-        for link in self.tutorial:
-            print(f"\t{link}")
 
 
 class MultipleChoiceQuestion(Question):
@@ -37,9 +24,8 @@ class MultipleChoiceQuestion(Question):
         prompt: str,
         answer: str,
         choices: Iterable[str],
-        tutorial: Union[str, Iterable[str]] = None,
     ):
-        super(MultipleChoiceQuestion, self).__init__(prompt, answer, tutorial)
+        super(MultipleChoiceQuestion, self).__init__(prompt, answer)
         self.choices = choices
         if self.answer not in self.choices:
             raise "Answer must be in choices"
