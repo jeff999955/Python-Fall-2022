649dbc4da9082f6058ff4d8b2047a19c3d948895
Jeff <jeff999955@gmail.com>
2022-12-12 15:01:17 +0800

    Add main program flow

--- main.py
+++ main.py
@@ -1,19 +1,55 @@
+import json
+from lesson import Lesson
 from question import MultipleChoiceQuestion
 
 
-question_list = [
-    MultipleChoiceQuestion(
-        prompt="print(type(0.56)) 顯示的結果為何?",
-        answer="float",
-        choices=["int", "float", "str", "bool"],
-        tutorial="https://youtu.be/P5C8u7diAYk",
-    ),
-]
-
-for question in question_list:
-    question.ask()
-    if question.validate():
-        print("Correct!")
+def select_lesson() -> Lesson:
+    print("Which lesson would you like to take?\n\t1. Lesson 1\n\t2. Lesson 2")
+    choice = int(input("Your choice: "))
+    if choice in [1, 2]:
+        with open(f"lesson{choice}.json") as fp:
+            return Lesson.from_json(json.load(fp))
     else:
-        print(f"Incorrect! The correct answer is {question.answer}.")
-        question.show_tutorial()
+        raise ValueError("Invalid choice")
+
+
+def start_lesson(lesson: Lesson) -> None:
+    print(f"Starting lesson {lesson.name}")
+    while True:
+        print("1. Show instructions")
+        print("2. Show questions")
+        print("3. Show tutorials")
+        print("4. Exit")
+        choice = input("Your choice: ")
+        if choice == "1":
+            lesson.show_instructions()
+        elif choice == "2":
+            lesson.show_questions()
+        elif choice == "3":
+            lesson.show_tutorials()
+        elif choice == "4":
+            break
+        else:
+            print("Invalid choice.")
+
+
+def main() -> None:
+    print("Welcome to the Python lesson!")
+    while True:
+        print("1. Start lesson")
+        print("2. Exit")
+        choice = input("Your choice: ")
+        if choice == "1":
+            try:
+                lesson = select_lesson()
+            except Exception:
+                print("Invalid choice for the lesson.")
+            start_lesson(lesson)
+        elif choice == "2":
+            break
+        else:
+            print("Invalid choice.")
+
+
+if __name__ == "__main__":
+    main()
